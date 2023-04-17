01bece039b0d8897dec2e659af8577fe6d0fd1bb
V <me123opportunity@gmail.com>
2023-04-16 21:52:56 -0400

    Test correlation of bedroom v price

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import sys
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 #send to output.txt
 # orig_stdout = sys.stdout
@@ -10,20 +11,15 @@ import matplotlib.pyplot as plt
 # sys.stdout = f
 # ##
 
-
-
-
 file_path = '/Users/v/Documents/Coursera/Data Analysis/kc_house_data.csv'
 df = pd.read_csv(file_path, header=None)
 headers = ['id','date','price','bedrooms','bathrooms','sqft_living','sqft_lot','floors','waterfront','view','condition','grade','sqft_above','sqft_basement','yr_built','yr_renovated','zipcode','lat','long','sqft_living15','sqft_lot15']
 df.columns = headers
+# df.drop(columns=['id'])
 
 
 df.replace("?", np.nan, inplace=True)
 
-#variables to bin -->
-# sqfeetliving, lot, above, basement 
-# print(df['price'])
 
 bins = np.linspace(min(df["sqft_living"]), max(df["sqft_living"]), 4)
 
@@ -32,17 +28,31 @@ group_names = ['Low', 'Medium', 'High']
 df['sqft_living-binned'] = pd.cut(df['sqft_living'], bins, labels=group_names, include_lowest=True )
 values = df['sqft_living-binned'].value_counts() 
 
-fig = plt.figure(figsize = (10, 5))
-plt.bar(group_names, values)
+# fig = plt.figure(figsize = (10, 5))
+# plt.bar(group_names, values, width=0.6)
+# plt.title('Living Sqft')
+# plt.xlabel('Sqft')
+# plt.ylabel('Count')
+
+print(df['id'])
+# print(df[['bedrooms', 'bathrooms', 'floors', 'grade']].corr())
+
+#
+df.describe().to_csv("house_stats.csv")
+
+
+
+sns.regplot(x='bedrooms', y='price', data=df, marker='o', color='red', scatter_kws={"s":2})
+plt.ylim(0,)
+plt.title('Bedrooms vs Price')
+print(df[['bedrooms','price']].corr())
+
 
 
 
 #send to output.txt   
 # sys.stdout = orig_stdout
 # f.close()
-
-
-
 
 # import matplotlib.pyplot as plt
 # height = [189, 185, 195, 149, 189, 147, 154, 
